05693fcdc22d3090ba7a5cfd6864c905b7706803
erlaniolla <erlan.chkalov007@gmail.com>
2025-12-01 16:52:01 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Main from "./components/Main";
+import Blog from "./pages/Blog/Blog";
 
 function App() {
   return (
     <>
-      <Header/>
-        
-      <Footer/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/" element={<Blog/>}/>
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className="container">
-      awdouabwidla
+    <div id="header">
+      <div className="container">
+        <div className="header">
+          <NavLink to='/'><h1>BARBER</h1></NavLink>
+          <div>
+            <NavLink to='/blog'>Блог</NavLink>
+            <NavLink to={'/about'}>Вакансий</NavLink>
+            <NavLink to={'/about'}>О нас</NavLink>
+          </div>
+          <a href="tel:+996555123456">
+            <h3>+996 (555) 12-34-56</h3>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
