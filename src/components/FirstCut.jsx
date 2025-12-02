3c84a2cf9b8114f966e6f4e9838b037e9d5dfce1
erlaniolla <erlan.chkalov007@gmail.com>
2025-12-02 14:15:15 +0600

    npm i

--- src/components/FirstCut.jsx
+++ src/components/FirstCut.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import img1 from '../assets/image/Main-pic.webp'
+import img2 from '../assets/image/main-pic2.webp'
 
 const FirstCut = () => {
   return (
@@ -8,6 +10,15 @@ const FirstCut = () => {
           <div>
             <p>Бишкек, Улица Шабдан баатыра 192/2</p>
             <h1>Создайте идеальный образ в нашем барбершопе!</h1>
+          </div>
+          <div>
+            <div className='first-cut-discount'>
+              <img src={img2} alt="" />
+              <div>
+                <h3>Скидка на первую стрижку у нас — 15%</h3>
+                <button className='btn' >Записаться</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FirstCut from './FirstCut'
+import { Services } from './Services'
 
 const Main = () => {
   return (
     <>
       <FirstCut/>
+      <Services/>
     </>
   )
 }
