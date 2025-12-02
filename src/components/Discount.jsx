646b608729665f5cd17f41480cbf3252742da490
Abivveb <ableshalykadyrov@gmail.com>
2025-12-02 16:30:55 +0600

    test: Discount

--- src/components/Discount.jsx
+++ src/components/Discount.jsx
@@ -5,14 +5,16 @@ import { FiArrowUpRight } from "react-icons/fi";
 export const Discount = () => {
   return (
     <div id="discount">
-        <div className="discount">
-            <div className="disc-img">
-                <img src={cutPic} alt="" />
-            </div>
-            <div className="discount-info">
-                <h4>Скидка на стрижку при первом посещении — 15%</h4>
-                <span>(Скидка не распространяется на топ барберов)</span>
-                <button className="btn disc-btn">Записаться <FiArrowUpRight size={24}/></button>
+        <div className="container disc-cont">
+            <div className="discount">
+                <div className="disc-img">
+                    <img src={cutPic} alt="" />
+                </div>
+                <div className="discount-info">
+                    <h4>Скидка на стрижку при первом посещении — 15%</h4>
+                    <span>(Скидка не распространяется на топ барберов)</span>
+                    <button className="btn disc-btn">Записаться <FiArrowUpRight size={24}/></button>
+                </div>
             </div>
         </div>
     </div>
