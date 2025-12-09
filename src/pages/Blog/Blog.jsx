081804ce7a41681f4278178f3253310d225c58d2
Abivveb <ableshalykadyrov@gmail.com>
2025-12-09 18:32:51 +0600

    created: Form in Job-added styles

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import blogImg from '../../assets/image/blogImage.jpeg'
 
 const Blog = () => {
+  const blogList = [
+    {id: 1, title: 'Blog News', image: blogImg, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.'},
+    {id: 2, title: 'Blog News', image: blogImg, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.'},
+    {id: 3, title: 'Blog News', image: blogImg, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.'},
+    {id: 4, title: 'Blog News', image: blogImg, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.'},
+    {id: 5, title: 'Blog News', image: blogImg, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.'},
+    {id: 6, title: 'Blog News', image: blogImg, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.'},
+    {id: 7, title: 'Blog News', image: blogImg, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In hendrerit gravida rutrum quisque non tellus orci ac auctor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Viverra adipiscing at in tellus.'},
+  ]
   return (
-    <div>
-      
+    <div id="blog">
+      <div className="container">
+        <div className="blog">
+          <div className="blog-upper"></div>
+          <div className="blog-grid">
+            {blogList.map(blog => (
+              <div className="blog-item" key={blog.id}>
+                  <div className="blog-img">
+                    <img src={blog.image} alt="blog-img" />
+                  </div>
+                  <h3 className='blog-title'>{blog.title}</h3>
+                  <p className="blog-text">{blog.text}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Job/Job.jsx
+++ src/pages/Job/Job.jsx
@@ -1,14 +1,54 @@
 import React from 'react'
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md"; 
 
 const Job = () => {
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  const [number, setNumber] = React.useState('')
+  const sendData = (e) => {
+    e.preventDefault()
+    console.log(
+      {
+        id: Date.now(),
+        name: name,
+        email: email,
+        number: number
+      }
+    )
+  }
   return (
-    <div>
-      <div>fsuigf</div>
-      <div>fsuigf</div>
-      <div>fsuigf</div>
-      <div>fsuigf</div>
-      <div>fsuigf</div>
+   <div id="job">
+    <div className="container">
+      <div className="job">
+        <div className="job-wrapper">
+          <h1 className="job-title">хочешь работать в команде?</h1>
+          <div className="job-form">
+            <div className="job-address">
+              <span className="job-gmail"><div className="job-icon"><MdOutlineMail size={24}/></div> Barber@gmail.com</span>
+              <span className="job-number"><div className="job-icon"><FaPhoneAlt size={24}/></div> +996 700-00-00-00</span>
+            </div>
+            <form id='form'>
+              <h2>Оставьте заявку и мы свяжемся с вами</h2>
+              <label htmlFor="">
+                <span>Ваше Имя</span>
+                <input type="text" placeholder='Ваше имя' onChange={(e) => setName(e.target.value)}/>
+              </label>
+              <label htmlFor="">
+                <span>Ваш Email</span>
+                <input type="text" placeholder='barber@gmail.com' onChange={(e) => setEmail(e.target.value)}/>
+              </label>
+              <label htmlFor="">
+                <span>Номер телефона</span>
+                <input type="text" placeholder='0700 00 00 00' onChange={(e) => setNumber(e.target.value)}/>
+              </label>
+              <button className='btn send-btn' onClick={(e) => sendData(e)}>Отправить</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
+   </div>
   )
 }
 
